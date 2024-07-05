7c1191ab4739fbb6b461d2fa1d4441689c1626dd
RamiroUrt <121266524+RamiroUrt@users.noreply.github.com>
2024-07-05 18:42:20 -0300

    Update LanguageOption.jsx

--- src/components/LanguageOption.jsx
+++ src/components/LanguageOption.jsx
@@ -2,17 +2,17 @@ import '../assets/LanguageOption.css';
 
 const LanguageOption = () => {
   return (
-<div className="wrapper">
+<div className="wrapper dark:-bg--light-black--">
   <div className="option">
     <input checked="" value="option1" name="btn" type="radio" className="input" />
     <div className="btn">
-      <span className="span">English</span>
+      <span className="span dark:-text--light-white">English</span>
     </div>
   </div>
   <div className="option">
     <input value="option2" name="btn" type="radio" className="input" />
     <div className="btn">
-      <span className="span">Spanish</span>
+      <span className="span dark:-text--light-white">Spanish</span>
     </div>
   </div>
 </div>
