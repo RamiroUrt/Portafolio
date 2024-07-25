61347aaf98a5da19edc2d3c265c2b238eec41290
RamiroUrt <urteagaramiro33@gmail.com>
2024-07-25 18:56:12 -0300

    img

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -13,7 +13,8 @@ const Header = () => {
       <Nav />
       <section id='profile' className="profile">
         <div className="section_pic">
-          <img src={Perfil} alt={t('profile.name')} />
+          <img src={Perfil} alt={t('profile.name')} className='dark:-z-[-11]'/>
+          <div className="back-pic dark:-bg--light-gray-- dark:-z-[-2]"></div>
         </div>
         <div className="section_text ">
           <p className="section_text_p dark:-text--light-white">{t('profile.hello')}</p>
