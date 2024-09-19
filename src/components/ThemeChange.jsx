407b04ec73cbb99097bdc1d5f397d9d3d059f7b5
RamiroUrt <urteagaramiro33@gmail.com>
2024-09-19 13:09:05 -0300

    fix bug buttons

--- src/components/ThemeChange.jsx
+++ src/components/ThemeChange.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import '../assets/ThemeChange.css';
 
 const ThemeChange = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   useEffect(() => {
     if (theme === 'dark') {
