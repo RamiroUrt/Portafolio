e03b7a6c929eff445e4441def6e7fadd30299cfb
RamiroUrt <urteagaramiro33@gmail.com>
2025-05-07 12:02:56 -0300

    config.mjs

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(
-    {
+  plugins: [
+    react({
       include: [/\.js$/, /\.jsx$/],
-    }
-  ), tailwindcss()],
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@lib': path.resolve(__dirname, './src/lib'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      '@styles': path.resolve(__dirname, './src/styles')
+      '@styles': path.resolve(__dirname, './src/styles'),
     }
   },
   optimizeDeps: {
