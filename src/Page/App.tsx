756f4a7ace64689036155fd69755e92085bd5920
Ramiro Urteaga <urteagaramiro33@gmail.com>
2026-05-20 11:09:00 -0300

    chore: update node engine version

--- src/Page/App.tsx
+++ src/Page/App.tsx
@@ -1,49 +1,49 @@
 import { useState } from "react";
 import BrowserWindow from "../components/layout/BrowserWindow/BrowserWindow";
 import Aside from "../components/layout/Sidebar/Sidebar";
-// import Loader from "../components/ui/Loader/Loader"; 
-// import AlertIOS from "../components/ui/Alert/Toast"; 
+import Loader from "../components/ui/Loader/Loader"; 
+import AlertIOS from "../components/ui/Alert/Toast"; 
 import { ThemeProvider } from "../context/ThemeContext"; 
 import { TabProvider } from '../context/tabContex';
 function App() {
-  // const [renderMain, setRenderMain] = useState(false);
-  // const [isLoaderVisible, setIsLoaderVisible] = useState(true);
-  // const [showTutorial, setShowTutorial] = useState(false); 
+  const [renderMain, setRenderMain] = useState(false);
+  const [isLoaderVisible, setIsLoaderVisible] = useState(true);
+  const [showTutorial, setShowTutorial] = useState(false); 
 
-  // const handleLoaderFinish = () => {
-  //   setRenderMain(true);
+  const handleLoaderFinish = () => {
+    setRenderMain(true);
 
-  //   requestAnimationFrame(() => {
-  //     setTimeout(() => {
-  //        setIsLoaderVisible(false);
+    requestAnimationFrame(() => {
+      setTimeout(() => {
+         setIsLoaderVisible(false);
         
-  //       setTimeout(() => {
-  //         setShowTutorial(true);
-  //       }, 500);
+        setTimeout(() => {
+          setShowTutorial(true);
+        }, 500);
 
-  //     }, 10); 
-  //   });
-  // };
+      }, 10); 
+    });
+  };
 
   return (
     <ThemeProvider>
       <TabProvider>
-      {/* {renderMain && ( */}
+      {renderMain && (
         <main className="main-layout">
           <Aside />
           <BrowserWindow />
           
-          {/* <AlertIOS 
+          <AlertIOS 
             show={showTutorial} 
             onClose={() => setShowTutorial(false)} 
             translationKey="welcome"
-          /> */}
+          />
         </main>
-       {/* )} */}
+       )}
 
-       {/* {isLoaderVisible && (
+       {isLoaderVisible && (
         <Loader onFinish={handleLoaderFinish} />
-      )}  */}
+      )} 
       </TabProvider>
     </ThemeProvider>
   );
